5f04a909682e465ed861dc98644db16e59895b0c
IshariAbeysooriya <abeysooriyaip.21@uom.lk>
2024-10-08 20:33:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App
